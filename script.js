de48bb246266ae4ab34b94bc3219cc6625cd86a9
mfaseeh500 <93811900+mfaseeh500@users.noreply.github.com>
2023-05-09 00:25:02 +0500

    9/5/2023 (added javascript validations for all input fields)

--- script.js
+++ script.js
@@ -3,25 +3,35 @@ function login(){
     var email=document.getElementById("input_email");
     var password=document.getElementById("input_password");
     var confirm_password=document.getElementById("input_confirm_password");
-    var username_regex= /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/;
+    var username_regex= /^[a-zA-Z]{6}[0-9]{2,}$/;
     var email_regex= /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var password_regex=/^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+=\[{\]}\|:;\"'<,>.?/])(?!.*\s).{8,}$/;
     if((username.value.trim()=="") || (email.value.trim()=="") || (password.value.trim()=="") || (confirm_password.value.trim()=="") ){
         window.alert("Fields shouldnot be left empty");
     }
     else{
         if(password.value.trim()==confirm_password.value.trim()){
-            if((username_regex.test(username.value.trim())) && (email_regex.test(email.value.trim()))){
-              return true;
+            if(username_regex.test(username.value.trim())){
+                if(email_regex.test(email.value.trim())){
+                    if( (password_regex.test(password.value.trim())) && (password_regex.test(confirm_password.value.trim()))){
+                        window.alert("User Successfully Registered")
+                    }
+                    else{
+                        window.alert("Password should have atleast 8 characters. One capital letter, one number and one special character is must")
+                    }
+
+                }else{
+                    window.alert("email not written properly");
+                }
             }
             else{
-                window.alert("username not valid");
+                window.alert("username should have at least 8 characters. First 6 characters should be numbers followed by numbers. Special characters are not allowed");
                 return false;
-
             }
 
         }
         else{
-            window.alert("password no match")
+            window.alert("passwords are not matching")
         }
 
     }
